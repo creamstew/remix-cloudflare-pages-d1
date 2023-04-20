cd331fd5c6cc32c303eda9e05ea84e6931c54773
creamstew <r2011o4223mq@gmail.com>
2023-04-21 07:38:03 +0900

    d1へのアクセスを確認

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,37 +1,35 @@
-import type { V2_MetaFunction } from "@remix-run/react";
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
-export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+type Customer = {
+  CustomerID: number;
+  CompanyName: string;
+  ContactName: string;
+};
+
+export const loader = async ({ context }: LoaderArgs) => {
+  const db = context.DB as D1Database;
+
+  const { results } = await db
+    .prepare("SELECT * FROM Customers")
+    .all<Customer>();
+
+  return json({
+    customers: results ?? [],
+  });
 };
 
 export default function Index() {
+  const { customers } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {customers.map((customer) => (
+          <li key={customer.CustomerID}>{customer.CompanyName}</li>
+        ))}
       </ul>
     </div>
   );
